437b72c52e063b34f3c7972541706243f1855ee8
jamil <jamil.saleh@student.moringaschool.com>
2023-02-10 23:36:30 +0300

    cokmpleted assignmen

--- looping.rb
+++ looping.rb
@@ -1,24 +1,35 @@
-def happy_new_year
-  # your code here
+def happy_new_year(countdown = 10)
+  until countdown == 0
+    puts "#{countdown}"
+    countdown -= 1
+end
+
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
-  end
+if num % 3 == 0 && num % 5 == 0
+  "FizzBuzz"
+elsif num % 3 == 0
+  "Fizz"
+elsif num % 5 == 0
+  "Buzz"
+else
+  num
+end
 end
 
-def fizzbuzz_printer
-  # your code here
+def fizzbuzz_printer( num = 1)
+  until num == 101
+  puts "#{fizzbuzz(num)}"
+  num += 1
+end
 end
 
 def reverse_string(str)
-  # your code here
+new_string = str.split("")
+reversed_string = []
+str.size.times { reversed_string << new_string.pop }
+reversed_string.join
 end
